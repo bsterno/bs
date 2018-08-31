6f402ecaa8b7c0b0fae6623bea845483a8bb2311
Brian Stern <brian@brickworksoftware.com>
2018-08-30 22:09:42 -0400

    Verbiage

--- src/components/GithubActivities/GithubActivities.js
+++ src/components/GithubActivities/GithubActivities.js
@@ -25,13 +25,30 @@ class GithubActivities extends Component {
 
   render() {
 
+    let defineVerbiage = (obj, term) => {
+      if (term.includes('Push')) {
+        return 'pushed code to '
+      } else if (term.includes('Watch')) {
+        return 'starred '
+      } else if (term.includes('Pull')) {
+        return 'created a pull request in '
+      } else if ( (obj.payload.ref_type === 'repository') && (term.includes('Create')) ) {
+        return 'created the repository ';
+      } else if ( ((obj.payload.ref_type === 'branch') || (obj.payload.ref_type === 'tag')) && (term.includes('Create')) ) {
+        return 'created a ' + obj.payload.ref_type + ' in ';
+      } else {
+        return term
+      }
+    }
+
     const ghEvents = Object.keys(this.state.events)
       .map(event => {
         let eventObj = this.state.events[event];
+        let typeVerbiage = defineVerbiage(eventObj, eventObj.type);
         return (
           <Fragment>
             <p className="col-12">Login: {eventObj.actor.login}</p>
-            <p className="col-12">Type: {eventObj.type}</p>
+            <p className="col-12">Type: {typeVerbiage}</p>
             <p className="col-12">Repo: {eventObj.repo.name}</p>
             <p className="col-12">Created at: {eventObj.created_at}</p>
           </Fragment>
