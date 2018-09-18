1267fe46ea9787dc134a6be058017ab8989ed0aa
Brian Stern <brian@brickworksoftware.com>
2018-09-18 14:04:02 -0400

    404 component to class

--- src/containers/404.js
+++ src/containers/404.js
@@ -1,23 +1,39 @@
 import { Router, Link } from 'react-static'
-import React from 'react'
+import React, { Component } from 'react'
 //
+import './404.css'
 
 const Foo = () => (
   <h1>Foo</h1>
 )
 
-const Blah = () => (
-  <h1>Blah</h1>
+const Bar = () => (
+  <h1>Bar</h1>
 )
 
 const arr = [
   <Foo />,
-  <Blah />
+  <Bar />
 ]
 
-export default () => (
-  <div>
-    <h1>404 - Oh no's! We couldn't find that page :(</h1>
-      {arr[Math.floor(Math.random()*arr.length)]}
-  </div>
-)
+class Page404 extends Component {
+  componentWillMount() {
+    document.body.classList.add('page-404');
+  }
+
+  componentWillUnmount() {
+    document.body.classList.remove('page-404');
+  }
+
+  render() {
+    return (
+      <section className="row">
+        <div className="twinkling"></div>
+        <h1>404 - Oh no's! We couldn't find that page :(</h1>
+        {arr[Math.floor(Math.random()*arr.length)]}
+      </section>
+    )
+  }
+}
+
+export default Page404;
