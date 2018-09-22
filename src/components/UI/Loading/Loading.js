72d2a3476dc603a96b76f7e82542dab6eb296cd2
Brian Stern <briansternhk@gmail.com>
2018-09-21 22:33:41 -0400

    Loading page

--- src/components/UI/Loading/Loading.js
+++ src/components/UI/Loading/Loading.js
@@ -1,11 +1,17 @@
 import React from 'react'
 //
 import loadingIcon from '../../../assets/images/duck.gif';
+import reactIcon from '../../../assets/images/icons/react.png';
 import './Loading.css'
 
 const loading = () => (
   <div className="load-screen">
-    <img src={loadingIcon} />
+    <div className="load-content">
+      <img className="load-icon" src={loadingIcon} />
+      <div className="built-with col-12">
+        <p>Built with React</p><img src={reactIcon} />
+      </div>
+    </div>
   </div>
 )
 
