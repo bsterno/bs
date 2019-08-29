311e6d2335beef6485a43e1e65ec835a76e5c461
Brian Stern <bstern@smartasset.com>
2019-08-29 17:31:09 -0400

    Incognito fun, new loading icons

--- src/components/UI/Loading/Loading.js
+++ src/components/UI/Loading/Loading.js
@@ -1,18 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 //
-import loadingIcon from '../../../assets/images/duck.gif';
 import reactIcon from '../../../assets/images/icons/react.png';
+import loadingDuck from '../../../assets/images/loading-icons/duck.gif';
+import loadingCat from '../../../assets/images/loading-icons/gangnam-cat.gif';
+import loadingMario from '../../../assets/images/loading-icons/mario.gif';
+import loadingDorito from '../../../assets/images/loading-icons/dorito.gif';
+import loadingPikachu from '../../../assets/images/loading-icons/pikachu.gif';
 import './Loading.css'
 
-const loading = () => (
-  <div className="load-screen">
-    <div className="load-content">
-      <img className="load-icon" src={loadingIcon} />
-      <div className="built-with col-12">
-        <p>Built with React</p><img src={reactIcon} />
-      </div>
-    </div>
-  </div>
-)
+const loadingIcons = [
+  loadingDuck,
+  loadingCat,
+  loadingMario,
+  loadingDorito,
+  loadingPikachu
+];
 
-export default loading;
+class Loading extends Component {
+    async detectIncognito() {
+        if ('storage' in navigator && 'estimate' in navigator.storage) {
+            const {usage, quota} = await navigator.storage.estimate();
+            console.log(`Using ${usage} out of ${quota} bytes.`);
+
+            if(quota < 120000000){
+                // Incognito
+                alert("Creeping around in incognito mode huh? Don't worry, your secret is safe with me 😈");
+                window.localStorage.setItem('shownCreeperAlert', true);
+            } else {
+                // Not Incognito
+            }
+        } else {
+            // Cannot detect
+        }
+    }
+
+    componentWillUnmount() {
+        if (!window.localStorage.getItem('shownCreeperAlert')) {
+          this.detectIncognito();
+        }
+    }
+
+    render() {
+        return (
+            <div className="load-screen">
+                <div className="load-content">
+                    <img className="load-icon" src={loadingIcons[Math.floor(Math.random()*loadingIcons.length)]} />
+                    <div className="built-with col-12">
+                        <p>Built with React</p><img src={reactIcon} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Loading;
