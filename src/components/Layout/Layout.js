f1d4068be26c477a50e8b21eea1f7807586453f9
Brian Stern <bstern@smartasset.com>
2021-08-07 17:04:30 -0400

    Loading fix and some minor changes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,7 @@
-import { Router, Link } from 'react-static'
 import React, { Component, Fragment } from 'react'
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 //
 import Routes from 'react-static-routes'
 import Header from '../Navigation/Header/Header.js'
-import Loading from '../UI/Loading/Loading.js'
 import Head from '../Head/Head.js'
 
 class Layout extends Component {
@@ -12,12 +9,6 @@ class Layout extends Component {
     return (
       <Fragment>
         <Head />
-        <ReactCSSTransitionGroup
-          transitionName="loading"
-          transitionEnterTimeout={0}
-          transitionLeaveTimeout={300}>
-          <Loading />
-        </ReactCSSTransitionGroup>
         <Header />
         <div className="content">
           <Routes />
