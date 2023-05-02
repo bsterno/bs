eda2c817e2c51ad0c7f3533aedd672ac91ad22d5
Brian Stern <briansternhk@gmail.com>
2023-05-02 15:00:51 -0400

    folio-update - update title

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -3,19 +3,17 @@ import { Head } from 'react-static'
 //
 import favicon from '../../assets/images/icons/code.png'
 
-const head = () => {
-  return (
+const head = () => (
     <Head>
       <html lang="en" />
-      <meta charset="utf-8" />
+      <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-      <title>Brian Stern | Developer</title>
+      <title>Brian Stern</title>
       <meta name="description" content="Bstern's site" />
       <link rel="icon" type="image/png" href={favicon} />
       <meta name="robots" content="index,follow" />
       <meta name="googlebot" content="index,follow" />
     </Head>
   )
-}
 
 export default head;
